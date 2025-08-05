bc80cdfa54c2d5e1f14ebc5655702c2f52fb18e6
Artemii <hapyakartemii@gmail.com>
2025-08-05 19:16:35 +0300

    MPL-27   add unit tests

---
+++ src/test/groovy/order/OrderServiceTest.groovy
@@ -0,0 +1,311 @@
+package order
+
+import com.dotdot.marketplace.order.dto.OrderRequestDto
+import com.dotdot.marketplace.order.entity.Order
+import com.dotdot.marketplace.order.entity.OrderStatus
+import com.dotdot.marketplace.order.repository.OrderRepository
+import com.dotdot.marketplace.order.service.OrderServiceImpl
+import com.dotdot.marketplace.orderitem.dto.OrderItemRequestDto
+import com.dotdot.marketplace.orderitem.entity.OrderItem
+import com.dotdot.marketplace.product.entity.Product
+import com.dotdot.marketplace.product.repository.ProductRepository
+import com.dotdot.marketplace.user.entity.User
+import com.dotdot.marketplace.user.repository.UserRepository
+import org.modelmapper.ModelMapper
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+
+class OrderServiceTest extends Specification {
+    def orderRepository = Mock(OrderRepository)
+    def userRepository = Mock(UserRepository)
+    def productRepository = Mock(ProductRepository)
+    def modelMapper = new ModelMapper()
+    def orderService = new OrderServiceImpl(orderRepository, userRepository, productRepository, modelMapper)
+
+    def "createOrder should create a new order"() {
+        given:
+        def userId = 1L
+        def productId = 2L
+        def orderRequest = new OrderRequestDto(
+                user: userId,
+                status: OrderStatus.PENDING,
+                orderItems: [
+                        new OrderItemRequestDto(productId: productId, quantity: 2)
+                ]
+        )
+        def user = new User(id: userId)
+        def product = new Product(id: productId, price: 50.0)
+        def savedOrder = new Order(id: 1L, totalPrice: 100.0, status: OrderStatus.PENDING, createdAt: LocalDateTime.now(), user: user)
+
+        userRepository.findById(userId) >> Optional.of(user)
+        productRepository.findById(productId) >> Optional.of(product)
+        orderRepository.save(_ as Order) >> savedOrder
+
+        when:
+        def result = orderService.createOrder(orderRequest)
+
+        then:
+        result.id == 1L
+        result.totalPrice == 100.0d
+        result.status == OrderStatus.PENDING
+    }
+
+    def "createOrder should throw exception if user not found"() {
+        given:
+        def orderRequest = new OrderRequestDto(
+                user: 99L,
+                status: OrderStatus.PENDING,
+                orderItems: [
+                        new OrderItemRequestDto(productId: 2L, quantity: 1)
+                ]
+        )
+
+        userRepository.findById(99L) >> Optional.empty()
+
+        when:
+        orderService.createOrder(orderRequest)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "createOrder should throw exception if product not found"() {
+        given:
+        def userId = 1L
+        def orderRequest = new OrderRequestDto(
+                user: userId,
+                status: OrderStatus.PENDING,
+                orderItems: [
+                        new OrderItemRequestDto(productId: 99L, quantity: 1)
+                ]
+        )
+        userRepository.findById(userId) >> Optional.of(new User(id: userId))
+        productRepository.findById(99L) >> Optional.empty()
+
+        when:
+        orderService.createOrder(orderRequest)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "createOrder should throw exception if order items are empty"() {
+        given:
+        def userId = 1L
+        def orderRequest = new OrderRequestDto(
+                user: userId,
+                status: OrderStatus.PENDING,
+                orderItems: []
+        )
+        userRepository.findById(userId) >> Optional.of(new User(id: userId))
+
+        when:
+        orderService.createOrder(orderRequest)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "getOrderById should return order when it exists"() {
+        given:
+        def orderRequest = new Order(
+                id: 1L,
+                status: OrderStatus.PENDING,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 1L),
+                orderItems: []
+        )
+
+        orderRepository.findById(1) >> Optional.of(orderRequest)
+
+        when:
+        def result = orderService.getOrderById(1L)
+
+        then:
+        result.id == 1l
+        result.status == OrderStatus.PENDING
+        result.userId == 1l
+        result.orderItems.isEmpty()
+    }
+
+    def "getOrderById should throw exception if order does not exist"() {
+        given:
+        orderRepository.findById(99L) >> Optional.empty()
+
+        when:
+        orderService.getOrderById(99L)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "getAllOrders should return all orders"() {
+        given:
+        def order1 = new Order(
+                id: 1L,
+                status: OrderStatus.PENDING,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 1L),
+                orderItems: [])
+        def order2 = new Order(id: 2L,
+                status: OrderStatus.COMPLETED,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 2L),
+                orderItems: [])
+        orderRepository.findAll() >> [order1, order2]
+
+        when:
+        def result = orderService.getAllOrders()
+
+        then:
+        result.size() == 2
+        result[0].id == 1L
+        result[1].id == 2L
+    }
+
+    def "updateOrderStatus should update order status"() {
+        given:
+        def orderId = 1L
+        def existingOrder = new Order(id: orderId,
+                status: OrderStatus.PENDING,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 1L),
+                orderItems: []
+        )
+        orderRepository.findById(orderId) >> Optional.of(existingOrder)
+
+        def updateOrder = new OrderRequestDto(
+                status: OrderStatus.COMPLETED,
+                user: 1L,
+                orderItems: [
+                        new OrderItemRequestDto(productId: 2L, quantity: 1)
+                ]
+        )
+        def product = new Product(id: 2L)
+        productRepository.findById(2L) >> Optional.of(product)
+
+        orderRepository.save(existingOrder) >> existingOrder
+        when:
+        def result = orderService.updateOrder(updateOrder, orderId)
+
+        then:
+        result.id == orderId
+        result.status == OrderStatus.COMPLETED
+        result.userId == 1L
+        result.orderItems.size() == 1
+    }
+
+    def "updateOrderStatus should throw exception if order does not exist"() {
+        given:
+        def orderId = 99L
+        orderRepository.findById(orderId) >> Optional.empty()
+
+        when:
+        orderService.updateOrder(new OrderRequestDto(), orderId)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "updateOrderStatus should throw exception if product not found"() {
+        given:
+        def orderId = 1L
+        def existingOrder = new Order(id: orderId,
+                status: OrderStatus.PENDING,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 1L),
+                orderItems: []
+        )
+        orderRepository.findById(orderId) >> Optional.of(existingOrder)
+        def updateOrder = new OrderRequestDto(
+                status: OrderStatus.COMPLETED,
+                user: 1L,
+                orderItems: [
+                        new OrderItemRequestDto(productId: 99L, quantity: 1)
+                ]
+        )
+        productRepository.findById(99L) >> Optional.empty()
+        when:
+        orderService.updateOrder(updateOrder, orderId)
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "deleteOrder should delete an order"() {
+        given:
+        def orderId = 1L
+        def existingOrder = new Order(id: orderId,
+                status: OrderStatus.PENDING,
+                createdAt: LocalDateTime.now(),
+                user: new User(id: 1L),
+                orderItems: []
+        )
+        orderRepository.existsById(orderId) >> Optional.of(existingOrder)
+
+        when:
+        orderService.deleteOrderById(orderId)
+        then:
+        1 * orderRepository.deleteById(orderId)
+    }
+
+    def "deleteOrder should throw exception if order does not exist"() {
+        given:
+        def orderId = 99L
+        orderRepository.existsById(orderId) >> Optional.empty()
+
+        when:
+        orderService.deleteOrderById(orderId)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "calculateTotalPrice should return the correct total price for an order"() {
+        given:
+        def orderItems = [
+                new OrderItem(priceAtPurchase: 50.0, quantity: 2),
+                new OrderItem(priceAtPurchase: 30.0, quantity: 1),
+        ]
+
+        when:
+        def totalPrice = orderService.calculateTotalPrice(orderItems)
+
+        then:
+        totalPrice == 130.0d
+    }
+
+    def "calculateTotalPrice should thrown IllegalArgumentException"() {
+        given:
+        def orderItems = []
+
+        when:
+        orderService.calculateTotalPrice(orderItems)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "validateStatus should throw exception for null"() {
+        given:
+        def invalidStatus = null
+
+        when:
+        orderService.validateStatus(invalidStatus)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "validateStatus should return without exception for valid status"() {
+        given:
+        def validStatus = OrderStatus.PENDING
+
+        when:
+        orderService.validateStatus(validStatus)
+
+        then:
+        noExceptionThrown()
+    }
+
+}
---
+++ src/test/groovy/user/UserServiceTest.groovy
@@ -0,0 +1,180 @@
+package user
+
+import com.dotdot.marketplace.exception.UserNotFoundException
+import com.dotdot.marketplace.user.dto.UserRequestDto
+import com.dotdot.marketplace.user.entity.User
+import com.dotdot.marketplace.user.repository.UserRepository
+import com.dotdot.marketplace.user.service.UserServiceImpl
+import org.modelmapper.ModelMapper
+import spock.lang.Specification
+
+class UserServiceTest extends Specification {
+    def userRepository = Mock(UserRepository)
+    def modelMapper = new ModelMapper()
+
+    def userService = new UserServiceImpl(userRepository, modelMapper)
+
+    def "createUser should save user"() {
+        given:
+        def user = new UserRequestDto(
+                login: "Test User",
+                fullName: "123",
+                password: "123"
+        )
+
+        userRepository.save(_ as User) >> { User u ->
+            u.id = 1
+            return u
+        }
+
+        when:
+        def result = userService.createUser(user)
+        then:
+        result.id == 1
+        result.login == "Test User"
+        result.fullName == "123"
+    }
+
+    def "createUser should throw exception if user already exists"() {
+        given:
+        def user = new UserRequestDto(
+                login: "Test User",
+                fullName: "123",
+                password: "123"
+        )
+
+        userRepository.findByLogin("Test User") >> Optional.of(new User())
+
+        when:
+        userService.createUser(user)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "getUserById should return user"() {
+        given:
+        def user = new User()
+        user.id = 1
+        user.login = "Test User"
+        user.fullName = "123"
+
+        userRepository.findById(1) >> Optional.of(user)
+
+        when:
+        def result = userService.getUserById(1)
+        then:
+        result.id == 1
+        result.login == "Test User"
+        result.fullName == "123"
+
+    }
+
+    def "getUserById  when dont exist"() {
+        given:
+        def userId = 9999l
+
+        userRepository.findById(userId) >> Optional.empty()
+
+        when:
+        userService.getUserById(userId)
+
+        then:
+        thrown(UserNotFoundException)
+
+    }
+
+    def "updateUser should update user"() {
+        given:
+        def userId = 1
+        def userRequest = new UserRequestDto(
+                login: "Updated User",
+                fullName: "Updated Full Name",
+                password: "newpassword"
+        )
+
+        def existingUser = new User()
+        existingUser.id = userId
+        existingUser.login = "Old User"
+        existingUser.fullName = "Old Full Name"
+
+        userRepository.findById(userId) >> Optional.of(existingUser)
+        userRepository.save(_ as User) >> { User u ->
+            u.id = userId
+            return u
+        }
+
+        when:
+        def result = userService.updateUser(userId, userRequest)
+
+        then:
+        result.id == userId
+        result.login == "Updated User"
+        result.fullName == "Updated Full Name"
+    }
+
+    def "updateUser should throw exception if user does not exist"() {
+        given:
+        def userId = 9999l
+        def userRequest = new UserRequestDto(
+                login: "Updated User",
+                fullName: "Updated Full Name",
+                password: "newpassword"
+        )
+
+        userRepository.findById(userId) >> Optional.empty()
+
+        when:
+        userService.updateUser(userId, userRequest)
+
+        then:
+        thrown(UserNotFoundException)
+    }
+
+    def "deleteUser should delete user"() {
+        given:
+        def userId = 1
+        def user = new User()
+        user.setId(userId)
+        userRepository.existsById(userId) >> Optional.of(user)
+
+        when:
+        userService.deleteUser(userId)
+
+        then:
+        1 * userRepository.deleteById(userId)
+    }
+
+    def "deleteUser should throw exception if user does not exist"() {
+        given:
+        def userId = 9999l
+
+        userRepository.findById(userId) >> Optional.empty()
+
+        when:
+        userService.deleteUser(userId)
+
+        then:
+        thrown(UserNotFoundException)
+    }
+
+    def "validatePassword should return true for correct password"() {
+        given:
+        def request = new UserRequestDto(
+                login: "John",
+                fullName: "john@example.com",
+                password: badPassword
+        )
+
+        when:
+        userService.createUser(request)
+
+        then:
+        def ex = thrown(IllegalArgumentException)
+        ex.message == "Password must not be empty"
+
+        where:
+        badPassword << [null, "", "   "]
+    }
+
+}
